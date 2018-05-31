374a8439d127e3962d34c96179f41b53d3c0a126
Julio <juliosteam9@gmail.com>
2018-05-30 21:22:26 -0300

    COMMIT MASTER

---
+++ Codigo Fonte/mybaby_1/Assets/Camera_persona/cam3player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam3player : MonoBehaviour
+{
+
+   
+    public GameObject cabeca;
+    public GameObject[] posicoes;
+    public int indice = 0;
+    public float VelocidadeDeMovimento = 2;
+    private RaycastHit hit;
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(cabeca.transform);
+        //checar se tem colisor
+        if (!Physics.Linecast(cabeca.transform.position, posicoes[indice].transform.position))
+        {
+            transform.position = Vector3.Lerp(transform.position, posicoes[indice].transform.position, VelocidadeDeMovimento * Time.deltaTime);
+            Debug.DrawLine(cabeca.transform.position, posicoes[indice].transform.position);
+        }
+        else if(Physics.Linecast(cabeca.transform.position, posicoes[indice].transform.position,out hit))
+         {
+        transform.position = Vector3.Lerp(transform.position, hit.point,(VelocidadeDeMovimento*2)*Time.deltaTime);
+        Debug.DrawLine(cabeca.transform.position, hit.point);
+        }
+
+      // void Update()
+       // {
+          //  if (Input.GetKeyDown("e") && (indice < posicoes.Length - 1))
+           // {
+              //  indice++;
+          //  }else if(Input.GetKeyDown("e") && indice >= (posicoes.Length - 1))
+           // {
+                //indice = 0;
+            //}
+       // }
+
+
+        }
+
+
+    }
+
+        
+    
+
+   
+
---
+++ Codigo Fonte/mybaby_1/Assets/Toon babies/Scripts/baby.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class baby : MonoBehaviour {
+    public float input;
+    public float sensitility = 3;
+    bool pressing;
+    public float rotateSpeed = 3.0f;
+    public float speed = 6.0f;
+
+    public CharacterController controller; // controlador do baby
+    public Animator animator; // controle de animaçoes
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        SetTransformX(0.021f);
+        Rotacionar();
+        Mover();
+        rodarEsquerda();
+        rodarDireita();
+        andarParaTras();
+
+        // var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        // var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
+        //float velocidadeDopersonagem = Mathf.Abs(x) + Mathf.Abs(z);
+        // animator.SetFloat("velocidade", velocidadeDopersonagem);
+
+
+    }
+    private void andarParaTras()
+    {
+        if (
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.PageDown)
+            )
+        {
+            animator.SetFloat("dandore", 2.0f);
+            SetTransformX(0.021f);
+        }
+        if (Input.GetKeyUp(KeyCode.S) ||
+                Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            animator.SetFloat("dandore", 0.0f);
+            SetTransformX(0.021f);
+
+        }
+    }
+
+
+
+
+    private void rodarEsquerda()
+    {
+        if (
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.LeftAlt)
+            )
+        {
+            animator.SetFloat("rodando", 2.0f);
+            SetTransformX(0.021f);
+        }
+        if (Input.GetKeyUp(KeyCode.A) ||
+                Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            animator.SetFloat("rodando", 0.0f);
+            SetTransformX(0.021f);
+
+        }
+    }
+
+        private void rodarDireita()
+        {
+            if (
+                Input.GetKey(KeyCode.D) ||
+                Input.GetKey(KeyCode.RightAlt)
+                )
+            {
+                animator.SetFloat("rodandodireita", 2.0f);
+                SetTransformX(0.021f);
+            }
+            if (Input.GetKeyUp(KeyCode.D) ||
+                    Input.GetKeyUp(KeyCode.RightAlt))
+            {
+                animator.SetFloat("rodandodireita", 0.0f);
+                SetTransformX(0.021f);
+
+            }
+
+        }
+        private void Mover()
+        {
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            transform.Translate(0, 0, z);
+            //Vector3 vetorVelocidade = transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            // controller.SimpleMove(vetorVelocidade);
+            if (
+                Input.GetKey(KeyCode.W) ||
+               Input.GetKey(KeyCode.PageUp)) {
+                SetTransformX(0.021f);
+                animator.SetFloat("velocidade", 2.0f);
+            }
+            if (Input.GetKeyUp(KeyCode.W) ||
+                Input.GetKeyUp(KeyCode.PageUp))
+        {
+            animator.SetFloat("velocidade", 0.0f);
+            SetTransformX(0.021f);
+            
+            }
+        }
+
+
+
+        private void Rotacionar()
+        {
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        }
+        void SetTransformX(float n)
+        {
+            transform.position = new Vector3(transform.position.x, n, transform.position.z);
+        }
+
+
+    }
+
+
+
+
+
+
